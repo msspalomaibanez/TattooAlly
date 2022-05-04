b67f0d4256e9aebc9748cc20439913f1a0b3204d
paloma <palibdev@gmail.com>
2022-05-04 20:20:50 +0200

    añadidas las interfaces correspondientes a las actividades

--- app/src/main/java/com/example/tattooally/SplashActivity.java
+++ app/src/main/java/com/example/tattooally/SplashActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class PerfilActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
